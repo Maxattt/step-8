f59a4a0cb0d5fd3916bb3c542c1bdb1a685838cf
Maxattt <113494886+Maxattt@users.noreply.github.com>
2025-02-05 04:23:11 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
+
+
+const App = () => {
+
+    const todoData = [
+        { label: 'Drink Coffee', important: false },
+        { label: 'Implement React Application', important: true },
+        { label: 'Make notes from your stud', important: false },
+        { label: 'Do not drink alchohol', important: false },
+    ];
+
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList todos =  {todoData} />
+        </div>
+    );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
